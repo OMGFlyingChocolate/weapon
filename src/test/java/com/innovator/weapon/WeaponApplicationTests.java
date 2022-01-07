5d751361c5432c0421279da856d110701c28fcba
曾文华 <47876476+OMGFlyingChocolate@users.noreply.github.com>
2022-01-07 14:38:08 +0800

    initialize

---
+++ src/test/java/com/innovator/weapon/WeaponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovator.weapon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
